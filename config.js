aa74ecce448d57b7f9eaf12c4e66277ce043ba16
anitdeshpandeethosh <anitdeshpandeethosh@gmail.com>
2026-03-12 11:21:49 +0530

    config
    config

---
+++ config.js
@@ -0,0 +1,6 @@
+// ===============================
+// 🔐 PUBLIC CONFIG
+// ===============================
+
+// Vapi public key injected here
+window.VAPI_PUBLIC_KEY = "a1c29cb3-95ce-4cb4-bcad-d90df153576d";

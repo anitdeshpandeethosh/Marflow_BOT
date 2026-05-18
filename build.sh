640e9ca3e71a0c2e5bc019e86e9522bad1d4a9ed
AnitEthosh <anit.deshpande@ethosh.com>
2026-05-18 11:58:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-sed -i "s|**VAPI_PUBLIC_KEY**|$VAPI_PUBLIC_KEY|g" config.js
+# Replace VAPI key
 
-sed -i "s|**ASSISTANT_EN**|$ASSISTANT_EN|g" config.js
+sed -i "s|**VAPI_PUBLIC_KEY**|${VAPI_PUBLIC_KEY}|g" config.js
 
-sed -i "s|**ASSISTANT_ES**|$ASSISTANT_ES|g" config.js
+# Replace assistant IDs
 
-sed -i "s|**ASSISTANT_AR**|$ASSISTANT_AR|g" config.js
+sed -i "s|**ASSISTANT_EN**|${ASSISTANT_ID_EN}|g" config.js
+
+sed -i "s|**ASSISTANT_ES**|${ASSISTANT_ID_ES}|g" config.js
+
+sed -i "s|**ASSISTANT_AR**|${ASSISTANT_ID_AR}|g" config.js
